77f17547f45572ee73f7d3d8f00944f0e3545289
Adrian7824 <93650838+Adrian7824@users.noreply.github.com>
2023-10-13 13:51:12 -0600

    Add files via upload

---
+++ spacex_dash_app.py
@@ -0,0 +1,84 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("c:/Users/52932/Downloads/spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='site-dropdown',
+        options=[{'label': 'All Sites', 'value': 'ALL'}] + [{'label': site, 'value': site} for site in spacex_df['Launch Site'].unique()],
+        value='ALL',
+        placeholder="Select a Launch Site here",
+        searchable=True
+    ),
+    dcc.RangeSlider(
+        id='payload-slider',
+        min=0,
+        max=10000,
+        step=1000,
+        value=[min_payload, max_payload]
+    ),
+    dcc.Graph(id='success-payload-scatter-chart'),
+    dcc.Graph(id='success-pie-chart')
+])
+@app.callback(
+    Output('success-pie-chart', 'figure'),
+    [Input('site-dropdown', 'value')]
+)
+def update_pie_chart(selected_site):
+    if selected_site == 'ALL':
+        # Mostrar un gráfico circular con el total de lanzamientos exitosos vs. fallidos en todos los sitios
+        total_success = len(spacex_df[spacex_df['class'] == 1])
+        total_failed = len(spacex_df[spacex_df['class'] == 0])
+
+        fig = px.pie(
+            names=['Success', 'Failure'],
+            values=[total_success, total_failed],
+            title='Total Launch Success',
+        )
+    else:
+        # Filtrar el DataFrame para el sitio de lanzamiento seleccionado
+        site_data = spacex_df[spacex_df['Launch Site'] == selected_site]
+        site_success = len(site_data[site_data['class'] == 1])
+        site_failed = len(site_data[site_data['class'] == 0])
+
+        fig = px.pie(
+            names=['Success', 'Failure'],
+            values=[site_success, site_failed],
+            title=f'Success/Failure for {selected_site}',
+        )
+
+    return fig
+@app.callback(
+    Output('success-payload-scatter-chart', 'figure'),
+    [Input('site-dropdown', 'value'), Input('payload-slider', 'value')]
+)
+def update_scatter_chart(selected_site, selected_payload_range):
+    if selected_site == 'ALL':
+        # Mostrar un gráfico de dispersión con Payload Mass (kg) vs. class (éxito/fracaso)
+        filtered_data = spacex_df[(spacex_df['Payload Mass (kg)'] >= selected_payload_range[0]) &
+                                  (spacex_df['Payload Mass (kg)'] <= selected_payload_range[1])]
+        fig = px.scatter(filtered_data, x='Payload Mass (kg)', y='class', color='Booster Version Category', title='Payload vs. Launch Outcome')
+    else:
+        # Filtrar el DataFrame para el sitio de lanzamiento seleccionado
+        site_data = spacex_df[spacex_df['Launch Site'] == selected_site]
+        filtered_data = site_data[(site_data['Payload Mass (kg)'] >= selected_payload_range[0]) &
+                                 (site_data['Payload Mass (kg)'] <= selected_payload_range[1])]
+        fig = px.scatter(filtered_data, x='Payload Mass (kg)', y='class', color='Booster Version Category', title=f'Payload vs. Launch Outcome for {selected_site}')
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
